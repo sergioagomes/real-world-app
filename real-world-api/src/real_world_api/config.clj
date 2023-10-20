f64d4b0dbef3a5fa493580a6b050e9161e7f8f6b
Sergio <thesergio.gomes@gmail.com>
2023-10-19 23:45:01 -0300

    wip: made read-config works

---
+++ real-world-api/src/real_world_api/config.clj
@@ -0,0 +1,12 @@
+(ns real-world-api.config
+  (:require [aero.core :as aero]
+            [clojure.java.io :as io]))
+
+
+(defn read-config
+  []
+  (-> "config.edn"
+      (io/resource)
+      (aero/read-config )))
+  
+
